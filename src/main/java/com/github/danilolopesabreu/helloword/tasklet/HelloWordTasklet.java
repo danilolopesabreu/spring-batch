46188bde13ec1cca14c5a0f01ba2e4f2287a25d6
danilolopesabreu <danilo.bcc@gmail.com>
2022-02-24 23:33:41 -0300

    add property parameter

--- src/main/java/com/github/danilolopesabreu/helloword/tasklet/HelloWordTasklet.java
+++ src/main/java/com/github/danilolopesabreu/helloword/tasklet/HelloWordTasklet.java
@@ -1,16 +1,20 @@
 package com.github.danilolopesabreu.helloword.tasklet;
 
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Qualifier("helloWordTasklet")
 @Component
+@StepScope
 public class HelloWordTasklet implements Tasklet {
 	
+	@Value("#{jobParameters['name']}")
 	private String name;
 	
 	public HelloWordTasklet() {}
@@ -23,7 +27,7 @@ public class HelloWordTasklet implements Tasklet {
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 //		System.out.println("Hello: "+this.name);
-		System.out.println("Hello !!...");
+		System.out.println("Hello !!..."+name);
 		return RepeatStatus.FINISHED;
 	}
 }
