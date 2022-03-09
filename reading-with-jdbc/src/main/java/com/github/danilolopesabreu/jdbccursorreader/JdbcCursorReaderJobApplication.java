72b33e2ac25715e99e1771f319cd5a229e4c0ebc
danilolopesabreu <danilo.bcc@gmail.com>
2022-03-08 23:52:42 -0300

    add reading from data base

---
+++ reading-with-jdbc/src/main/java/com/github/danilolopesabreu/jdbccursorreader/JdbcCursorReaderJobApplication.java
@@ -0,0 +1,13 @@
+package com.github.danilolopesabreu.jdbccursorreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcCursorReaderJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcCursorReaderJobApplication.class, args);
+	}
+
+}
