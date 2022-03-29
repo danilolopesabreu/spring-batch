a1afda475aeeb03e4673ac63795502c2e3fa96ad
danilolopesabreu <danilo.bcc@gmail.com>
2022-03-28 22:24:18 -0300

    ajustes.

---
+++ skip-exception-job/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.cliente VALUES ('Joao', 'Silva', '30', 'joao@test.com');
+INSERT INTO cliente VALUES ('Maria', 'Silva', '30', 'maria@test.com');
+INSERT INTO public.cliente(nome, sobrenome, idade, email) VALUES('Joao', 'Silva', '30', 'joao@test.com');
