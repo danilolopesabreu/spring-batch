545e7756efc523344ab41b13db84557464c340ff
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-14 16:22:22 -0300

    componente to skip

--- file-delimited-and-fixed/src/main/java/com/github/danilolopesabreu/filereader/fixedwidth/reader/SkipHeaderAndFooter.java
+++ file-delimited-and-fixed/src/main/java/com/github/danilolopesabreu/filereader/fixedwidth/reader/SkipHeaderAndFooter.java
@@ -1,9 +1,11 @@
 package com.github.danilolopesabreu.filereader.fixedwidth.reader;
 
 import org.springframework.batch.item.file.LineCallbackHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SkipHeaderAndFooter implements LineCallbackHandler {
-
+	
 	@Override
 	public void handleLine(String line) {
 		System.out.println(line);
