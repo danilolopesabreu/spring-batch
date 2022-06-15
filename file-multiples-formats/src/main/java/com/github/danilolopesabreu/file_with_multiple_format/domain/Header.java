48b9695020a25ef6306d6ac6b42473b0decc4765
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-14 23:20:06 -0300

    add properties

--- file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/domain/Header.java
+++ file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/domain/Header.java
@@ -1,5 +1,38 @@
 package com.github.danilolopesabreu.file_with_multiple_format.domain;
 
 public class Header {
+	private String tipoRegistro;
+	private String nomeHeader;
+	private int numeroHeader;
 
+	public String getTipoRegistro() {
+		return tipoRegistro;
+	}
+
+	public void setTipoRegistro(String tipoRegistro) {
+		this.tipoRegistro = tipoRegistro;
+	}
+
+	public String getNomeHeader() {
+		return nomeHeader;
+	}
+
+	public void setNomeHeader(String nomeHeader) {
+		this.nomeHeader = nomeHeader;
+	}
+
+	public int getNumeroHeader() {
+		return numeroHeader;
+	}
+
+	public void setNumeroHeader(int numeroHeader) {
+		this.numeroHeader = numeroHeader;
+	}
+
+	@Override
+	public String toString() {
+		return "Header [tipoRegistro=" + tipoRegistro + ", nomeHeader=" + nomeHeader + ", numeroHeader=" + numeroHeader
+				+ "]";
+	}
+	
 }
--- file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/domain/Trailler.java
+++ file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/domain/Trailler.java
@@ -1,5 +1,28 @@
 package com.github.danilolopesabreu.file_with_multiple_format.domain;
 
 public class Trailler {
+	private String tipoRegistro;
+	private int numeroTrailler;
+
+	public String getTipoRegistro() {
+		return tipoRegistro;
+	}
+
+	public void setTipoRegistro(String tipoRegistro) {
+		this.tipoRegistro = tipoRegistro;
+	}
+
+	public int getNumeroTrailler() {
+		return numeroTrailler;
+	}
+
+	public void setNumeroTrailler(int numeroTrailler) {
+		this.numeroTrailler = numeroTrailler;
+	}
+
+	@Override
+	public String toString() {
+		return "Trailler [tipoRegistro=" + tipoRegistro + ", numeroTrailler=" + numeroTrailler + "]";
+	}
 
 }
