d274b1af648f4d9cf4ba1bc2ff84da524f8ad9cf
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-15 22:07:51 -0300

    ajuste no step

--- file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/step/LeituraArquivoMultiplosFormatosStepConfig.java
+++ file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/step/LeituraArquivoMultiplosFormatosStepConfig.java
@@ -2,12 +2,15 @@ package com.github.danilolopesabreu.file_with_multiple_format.step;
 
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.danilolopesabreu.file_with_multiple_format.domain.ClientesAgrupados;
+import com.github.danilolopesabreu.file_with_multiple_format.processor.ProcessadorConfig;
 import com.github.danilolopesabreu.file_with_multiple_format.reader.ArquivoClienteTransacaoReader;
 
 @Configuration
@@ -15,14 +18,17 @@ public class LeituraArquivoMultiplosFormatosStepConfig {
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
 
+	@SuppressWarnings({"unchecked", "rawtypes"})
 	@Bean
 	public Step leituraArquivoMultiplosFormatosStep(
 			FlatFileItemReader arquivoMultiplosFormatosItemReader,
+			ItemProcessor processarClientesAgrupados,
 			ItemWriter leituraArquivoMultiplosFormatosItemWriter) {
 		return stepBuilderFactory
 				.get("leituraArquivoMultiplosFormatosStep")
-				.chunk(1)
+				.chunk(20)
 				.reader(new ArquivoClienteTransacaoReader(arquivoMultiplosFormatosItemReader))
+				.processor(processarClientesAgrupados)
 				.writer(leituraArquivoMultiplosFormatosItemWriter)
 				.build();
 	}
