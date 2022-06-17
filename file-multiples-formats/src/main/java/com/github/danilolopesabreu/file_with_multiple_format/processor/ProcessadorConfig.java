3697f806a2667a47fc5780c544757fcd52e51d0d
danilolopesabreu <danilo.bcc@gmail.com>
2022-06-16 23:28:09 -0300

    ajuste no processor

--- file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/processor/ProcessadorConfig.java
+++ file-multiples-formats/src/main/java/com/github/danilolopesabreu/file_with_multiple_format/processor/ProcessadorConfig.java
@@ -1,6 +1,9 @@
 package com.github.danilolopesabreu.file_with_multiple_format.processor;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +13,22 @@ import com.github.danilolopesabreu.file_with_multiple_format.domain.ClientesAgru
 public class ProcessadorConfig {
 
 	@Bean
-	public ItemProcessor<ClientesAgrupados, ClientesAgrupados> processarClientesAgrupados(ClientesAgrupados item) throws Exception {
-		System.out.println(" processando - "+item);
-		return null;
+	public ItemProcessor<ClientesAgrupados, ClientesAgrupados> processarClientesAgrupados() throws Exception {
+		System.out.println(" processando - ");
+		ValidatingItemProcessor<ClientesAgrupados> processor = new ValidatingItemProcessor<ClientesAgrupados>();
+		processor.setValidator(validator());
+		return processor;
+	}
+
+	private Validator<ClientesAgrupados> validator() {
+		return new Validator<ClientesAgrupados>() {
+
+			@Override
+			public void validate(ClientesAgrupados clienteAgrupado) throws ValidationException {
+				System.out.println(" validando "+clienteAgrupado);
+			}
+			
+		};
 	}
 
 }
